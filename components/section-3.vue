7993eca4ea3518dbd5d71b02fbbb39ce7d11c233
Devayani Dinda <dinda@tenggaraindonesia.id>
2025-06-08 16:44:43 +0800

    slicing-section-3

---
+++ components/section-3.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center justify-center text-center">
+        <h2 class="uppercase text-white mx-auto">
+            Work In <span class="text-syn-blue-500">synergy</span> with AI
+        </h2>
+        <div class="flex flex-col lg:grid grid-cols-2 gap-5 lg:mt-20 mt-10 items-center">
+            <div class="flex flex-col lg:w-[70%] gap-[30px]">
+                <div class="flex flex-row text-left h-[200px]">
+                    <div class="border-l-2 border-syn-blue-500 mr-5"></div>
+                    <div class="flex flex-col gap-4">
+                        <h3 class="text-white">ADVANCED AI ASSISTANT</h3>
+                        <p class="text-white/50 body-medium-normal">This paragraph mainly talk about our advanced
+                            and smarter AI that are scaleable and seamless switch between AI and real CS</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col gap-[30px] text-left justify-self-end lg:mt-0 mt-5 z-50">
+                <img class="flex rounded-lg h-fit w-full" src="/img/img-section-3.webp" alt="img-3" />
+            </div>
+        </div>
+    </div>
+</template>
